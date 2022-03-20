121a9f0028d0b3f597121fe610ed3086cbb199a7
eddiebond <eddiebond@outlook.com>
2022-03-20 18:19:24 +0000

    gantt chart now works

--- client/src/components/pages/chart.js
+++ client/src/components/pages/chart.js
@@ -2,10 +2,17 @@ import React from "react";
 import "../../App.css";
 
 function GanttChart() {
+
+    const ganttHTML = '<button onclick="img()">Click me!</button>\n' + '<div id = "container"></div>'
+
   return (
-    <div className="container">
-      <h1>Gantt Chart</h1>
-    </div>
+
+      <div className="container">
+          <button onClick={img}>Click me!</button>
+          <div onLoad={img} className={"ganttChart"} id={"container"}/>
+      </div>
+
+
   );
 }
 
